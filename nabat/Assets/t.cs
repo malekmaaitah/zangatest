ec5647f446019f0b52db162d17bd4a91ccea3ffd
malekmaaitah <malekalmaaytah@gmail.com>
2024-03-01 15:41:41 +0300

    ss

---
+++ nabat/Assets/t.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class t : MonoBehaviour
+{
+    public float ass;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
